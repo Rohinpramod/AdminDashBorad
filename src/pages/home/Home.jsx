2dd3950238bf1d506ea1474365879be1b01c44d3
ROHIN PRAMOD <rohinpramod320@gmail.com>
2024-12-20 17:12:00 +0530

    final commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,30 +8,3 @@ const Home = () => {
 
 export default Home;
 
-
-{/* <div>
-<div className="container mx-6 flex flex-col gap-10  ">
-  <div>
-    <Link to="/allrestaurants">
-      <button className="bg-gray-800 text-white p-2 rounded-lg mt-3">
-        All Restaurants
-      </button>
-    </Link>
-  </div>
-  <div>
-    <button className="bg-gray-800 text-white p-2 rounded-lg mt-3">
-      All MenuItems
-    </button>
-  </div>
-  <div>
-    <button className="bg-gray-800 text-white p-2 rounded-lg mt-3">
-      All Coupons
-    </button>
-  </div>
-  <div>
-    <button className="bg-gray-800 text-white p-2 rounded-lg mt-3">
-      All Resturants Orders
-    </button>
-  </div>
-</div>
-</div> */}
